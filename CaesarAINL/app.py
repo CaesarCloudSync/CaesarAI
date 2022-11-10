491969b21e68b9aa3d8aa155605c7668e1ed0fc3
CaesarCloudSync <56351085+CaesarCloudSync@users.noreply.github.com>
2022-11-10 15:24:34 +0000

    Update app.py

--- CaesarAINL/app.py
+++ CaesarAINL/app.py
@@ -1,22 +1,26 @@
 from flask import Flask,request
 from flask_cors import cross_origin
 from caesarinfer import CaesarNL
-app = Flask(__name__)
+import tensorflow as tf
+#app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
-@cross_origin()
-def caesarhome():
-    return "Caeser: How can I help you sir?"
 
-@app.route("/caesarapi",methods=["POST","GET"])
-@cross_origin()
-def caesarapi():
-    if request.method == "GET":
-        return "Caeser: Hello sir, this is the CaesarAIAPI"
-    elif request.method == "POST":
-        user_input_json = request.get_json()
-        caesarResponse,intents = CaesarNL.run([user_input_json["caesarapi"]])
-        return {"caesarmessage":{"caesarResponse":caesarResponse,"intent":intents}}
+#@app.route("/",methods=["GET"])
+#@cross_origin()
+#def caesarhome():
+#    return "Caeser: How can I help you sir?"
+
+#@app.route("/caesarapi",methods=["POST","GET"])
+#@cross_origin()
+#def caesarapi():
+#    if request.method == "GET":
+#        return "Caeser: Hello sir, this is the CaesarAIAPI"
+#    elif request.method == "POST":
+#       user_input_json = request.get_json()
+#        caesarResponse,intents = CaesarNL.run([user_input_json["caesarapi"]])
+#       return {"caesarmessage":{"caesarResponse":caesarResponse,"intent":intents}}
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=5000) # 
+    print(tf.__version__)
+    
+    #app.run(debug=True,host="0.0.0.0",port=5000) # 
